b89789a8319665d5e37eb0408061b99a71157de0
bakdongho <ehdgh4875@gmail.com>
2022-05-15 16:39:59 +0900

    Add testcase

--- gpc_api/map/tests.py
+++ gpc_api/map/tests.py
@@ -1,4 +1,14 @@
 import pytest
+from .naver_scrapper import NaverScrapper
 # Create your tests here.
 
-@pytest.fixture()
+@pytest.fixture
+def scrapper():
+    return NaverScrapper()
+
+def test_get_store(scrapper):
+    # Given: 위도, 경도
+    lng, lat = 37.5142950, 127.0626243
+
+    stores = scrapper.run(lng, lat)
+    assert isinstance(stores, list)
